6aa43518c175b9ea4f523a45d019505d74a3705c
plc <te2T@te>
2025-03-16 00:24:29 -0300

    first commit

---
+++ src/main/kotlin/br/com/paulocosta/f3dmanager/F3dManagerApplication.kt
@@ -0,0 +1,11 @@
+package br.com.paulocosta.f3dmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class F3dManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<F3dManagerApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/paulocosta/f3dmanager/F3dManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.paulocosta.f3dmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class F3dManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
